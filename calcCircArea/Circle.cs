1a9f4f58377bbc57a7957c5a1bc51041bae82adc
tundelola <foladayo@aol.com>
2016-11-05 16:24:18 -0400

    Innitial Commit

---
+++ calcCircArea/Circle.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calcCircArea
+{
+    class Circle
+    {
+        private double radius;
+        private object πr;
+        
+
+        public Circle()
+        {
+            //creating a constructor
+
+        }
+
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+
+            set
+            {
+                radius = value;
+            }
+        }
+        
+
+        public Circle(double rad)
+        {
+            radius = rad;//This is when it makes the new radius
+
+        }
+        public double getCircumference()
+        {
+            return Math.PI*2*Radius;// gets the circumsference of a radius
+
+        }
+        public double getArea()
+        {
+            return Math.PI*Radius*Radius;// gets the area of a radius
+           
+
+        }
+
+        private String formatNumber(double radi)
+        {
+        
+            return  Math.Round(radius, 2).ToString();//format radius number passed in
+        }
+
+        public String getFormatedCircumference()
+        {
+
+            return formatNumber(getCircumference());//format the circumsference of a radius
+        }
+
+        public String getFormatedArea()
+        {
+
+           return  formatNumber(getArea());//format the area of a radius
+
+        }
+
+        
+    }
+
+}
---
+++ calcCircArea/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calcCircArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           // Circle radius = new Circle();//Innitialize class object for readius object
+            
+
+            
+        }
+    }
+}
